2139abcc53325c25cb9dc47434fa6932a804297e
Wang-ChangTao <95759125+Wang-ChangTao@users.noreply.github.com>
2022-06-08 13:26:06 +0800

    test edit online

--- HelloWord/src/main/java/com/wct/App.java
+++ HelloWord/src/main/java/com/wct/App.java
@@ -12,7 +12,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!, This is a remark!!!" );
         SpringApplication.run(App.class, args);
     }
 }
